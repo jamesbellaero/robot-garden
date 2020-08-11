fa19e4a23cdbad8e82561d7b38dec8c094bb39b5
James Bell <jtb2013@gmail.com>
2020-08-10 22:27:40 -0600

    Added comments

--- logging/logging.py
+++ logging/logging.py
@@ -1,6 +1,27 @@
 import serial
 
-s = serial.Serial('/dev/ttyUSB0') # Open
+# Read in command line arguments
 
 
-ser.close
+# Open port
+s = serial.Serial('/dev/ttyUSB0') 
+
+# Begin reading data
+
+
+# Parse data
+
+
+# Either create a new file or write to an existing one
+
+
+# On error, attempt to reopen port
+
+
+
+
+
+
+
+
+ser.close()
