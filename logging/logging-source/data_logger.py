12ac22f78a7a2b50809b9a6555984f1fadc295a5
James Bell <james.bell@tautechnologies.com>
2020-10-11 20:55:30 -0600

    Started filling out the data logger

--- logging/logging-source/data_logger.py
+++ logging/logging-source/data_logger.py
@@ -2,4 +2,18 @@
 
 class DataLogger:
     def init(self):
-        self.
+        self.file_handles = []
+        # Easily searchable lists
+        self.directory_list = []
+        self.file_list = []
+    
+    def log(self, measurement):
+        # Create a new directory if necessary
+        if(not directory_list.contains(measurement.source)):
+            pass
+        # Create a new file if necessary
+        if(not file_list.contains(measurement.source + measurement.meas_type))
+            pass
+        # Write to file
+        
+
