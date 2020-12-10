c9cc7ea40c761ef28bc5bf0510fd90c53ff96819
James Bell <james.bell@tautechnologies.com>
2020-12-09 19:03:47 -0700

    Updated data logger

--- logging/logging-source/data_logger.py
+++ logging/logging-source/data_logger.py
@@ -1,8 +1,7 @@
-
+import os
 
 class DataLogger:
     def init(self):
-        self.file_handles = []
         # Easily searchable lists
         self.directory_list = []
         self.file_list = []
@@ -10,10 +9,29 @@ class DataLogger:
     def log(self, measurement):
         # Create a new directory if necessary
         if(not directory_list.contains(measurement.source)):
-            pass
+            self.directory_list.append(measurement.source)
+            self.file_list[measurement.source] = []
+            os.mkdir(measurement.source, 0o777) 
         # Create a new file if necessary
-        if(not file_list.contains(measurement.source + measurement.meas_type))
-            pass
+        if(not file_list[measurement.source].contains(measurement.meas_type)):
+            fh = open(measurement.source + "/" + measurement.meas_type + ".txt", "w")
+            self.file_list[measurement.source].append((measurement.meas_type,fh)
         # Write to file
+        fh = None
+        handle_list = self.file_list[measurement.source]
+        for file, handle in handle_list:
+            if(file == measurement.meas_type):
+                fh = handle
+
+        if(fh):
+            fh.write(measurement.time)
+            fh.write(",")
+            i = 0
+            for v in measurement.value:
+                fh.write(v)
+                if(i < len(measurement.value)-1):
+                    fh.write(",")
+                i=i+1
+        
         
 
