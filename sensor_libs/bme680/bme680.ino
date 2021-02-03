ca6b2fcd5b6e5625b11cd9f4d67dda8102e4f557
James Bell <james.bell@tautechnologies.com>
2021-02-03 09:50:16 -0700

    Doesn't write checksum yet for some reason

--- sensor_libs/bme680/bme680.ino
+++ sensor_libs/bme680/bme680.ino
@@ -11,6 +11,8 @@ uint16_t meas_period;
 struct bme680_dev gas_sensor;
 TwoWire *bmeWire = NULL;
 
+CrcSerializer serializer;
+
 
 /** Type definitions */
 /*!
@@ -77,55 +79,58 @@ void setup() {
   Serial.begin(9600);
   bmeWire = &Wire;
   bmeWire->begin();
-  
-  // Suggested defaults from Bosch  
-  gas_sensor.dev_id = BME680_I2C_ADDR_PRIMARY;
-  gas_sensor.intf = BME680_I2C_INTF;
-  gas_sensor.read = &readI2CPin;
-  gas_sensor.write = &writeI2CPin;
-  gas_sensor.delay_ms = &delay_msec; 
-  // amb_temp can be set to 25 prior to configuring the gas sensor 
-  // or by performing a few temperature readings without operating the gas sensor.
-   
-  gas_sensor.amb_temp = 21;
 
-  gas_sensor.tph_sett.os_hum = BME680_OS_2X;
-  gas_sensor.tph_sett.os_pres = BME680_OS_4X;
-  gas_sensor.tph_sett.os_temp = BME680_OS_8X;
-  gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
+  serializer = CrcSerializer();
+  serializer.InitializeCrcTable();
   
-  // Set the remaining gas sensor settings and link the heating profile 
-  //gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
-  // Create a ramp heat waveform in 3 steps 
-  gas_sensor.gas_sett.heatr_temp = 320; // degree Celsius 
-  gas_sensor.gas_sett.heatr_dur = 150; // milliseconds 
-  
-  // Select the power mode 
-  // Must be set before writing the sensor configuration 
-  gas_sensor.power_mode = BME680_FORCED_MODE; 
-  
-  
-  int8_t rslt = BME680_OK;
-  Serial.print(null_ptr_check(&gas_sensor));
-  Serial.print("Initializing...\n");
-  
-  rslt = bme680_init(&gas_sensor);
-  Serial.print("Result: ");
-  Serial.println(rslt);
-  
-  uint16_t meas_period;
-  bme680_get_profile_dur(&meas_period, &gas_sensor);
-
-  
-  // Set the required sensor settings needed 
-  uint8_t set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL 
-      | BME680_GAS_SENSOR_SEL;
-  
-  // Set the desired sensor configuration 
-  rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
-  
-  // Set the power mode 
-  rslt = bme680_set_sensor_mode(&gas_sensor);
+//  // Suggested defaults from Bosch  
+//  gas_sensor.dev_id = BME680_I2C_ADDR_PRIMARY;
+//  gas_sensor.intf = BME680_I2C_INTF;
+//  gas_sensor.read = &readI2CPin;
+//  gas_sensor.write = &writeI2CPin;
+//  gas_sensor.delay_ms = &delay_msec; 
+//  // amb_temp can be set to 25 prior to configuring the gas sensor 
+//  // or by performing a few temperature readings without operating the gas sensor.
+//   
+//  gas_sensor.amb_temp = 21;
+//
+//  gas_sensor.tph_sett.os_hum = BME680_OS_2X;
+//  gas_sensor.tph_sett.os_pres = BME680_OS_4X;
+//  gas_sensor.tph_sett.os_temp = BME680_OS_8X;
+//  gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
+//  
+//  // Set the remaining gas sensor settings and link the heating profile 
+//  //gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
+//  // Create a ramp heat waveform in 3 steps 
+//  gas_sensor.gas_sett.heatr_temp = 320; // degree Celsius 
+//  gas_sensor.gas_sett.heatr_dur = 150; // milliseconds 
+//  
+//  // Select the power mode 
+//  // Must be set before writing the sensor configuration 
+//  gas_sensor.power_mode = BME680_FORCED_MODE; 
+//  
+//  
+//  int8_t rslt = BME680_OK;
+//  Serial.print(null_ptr_check(&gas_sensor));
+//  Serial.print("Initializing...\n");
+//  
+//  rslt = bme680_init(&gas_sensor);
+//  Serial.print("Result: ");
+//  Serial.println(rslt);
+//  
+//  uint16_t meas_period;
+//  bme680_get_profile_dur(&meas_period, &gas_sensor);
+//
+//  
+//  // Set the required sensor settings needed 
+//  uint8_t set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL 
+//      | BME680_GAS_SENSOR_SEL;
+//  
+//  // Set the desired sensor configuration 
+//  rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
+//  
+//  // Set the power mode 
+//  rslt = bme680_set_sensor_mode(&gas_sensor);
 }
 
 void loop() {
@@ -133,19 +138,22 @@ void loop() {
   // More Bosch code
   delay(meas_period); // Delay till the measurement is ready 
 
-  struct bme680_field_data data;
-  int8_t rslt = bme680_get_sensor_data(&data, &gas_sensor);
-
-  serializeJsonMeasurement(Serial, data.temperature/100.0f,"Temperature","Celsius");
-  serializeJsonMeasurement(Serial, data.pressure*10.0f,"Pressure","Pascals"); // divide by 100 to get hPa
-  serializeJsonMeasurement(Serial, data.humidity/1000.0f,"Humidity","Relative Humidity");
-  
-  if(data.status & BME680_GASM_VALID_MSK)
-  {
-    serializeJsonMeasurement(Serial, data.gas_resistance,"Gas Resistance","Ohms");
-  }
-  
-  if (gas_sensor.power_mode == BME680_FORCED_MODE) {
-      rslt = bme680_set_sensor_mode(&gas_sensor);
-  }
+  serializer.SerializeJsonMeasurement(Serial, 50, "temp", "cels");
+//
+//  struct bme680_field_data data;
+//  int8_t rslt = bme680_get_sensor_data(&data, &gas_sensor);
+//
+//
+//  serializer.SerializeJsonMeasurement(Serial, data.temperature/100.0f,"Temperature","Celsius");
+//  serializer.SerializeJsonMeasurement(Serial, data.pressure*10.0f,"Pressure","Pascals"); // divide by 100 to get hPa
+//  serializer.SerializeJsonMeasurement(Serial, data.humidity/1000.0f,"Humidity","Relative Humidity");
+//  
+//  if(data.status & BME680_GASM_VALID_MSK)
+//  {
+//    serializer.SerializeJsonMeasurement(Serial, data.gas_resistance,"Gas Resistance","Ohms");
+//  }
+//  
+//  if (gas_sensor.power_mode == BME680_FORCED_MODE) {
+//      rslt = bme680_set_sensor_mode(&gas_sensor);
+//  }
 }
